33040bb12d46d918a47019c780d23c32f1089446
Angel Murchison <angelmurchison09@gmail.com>
2016-12-18 00:23:48 -0500

    created clerk customers views and controllers, created movie view model for admin page, created admin index.

---
+++ Primetime/Models/MovieViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Primetime.Models
+{
+    public class MovieViewModel
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int? genreID { get; set; }
+        public bool? isCheckedOut { get; set; }
+        public string genreName { get; set; }
+    }
+}
